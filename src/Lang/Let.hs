12bd12303261dceef478e129ec2d602fb6bf1201
Liu Enze <liuenze6516@gmail.com>
2018-07-04 23:35:32 +0800

    完成解析，暂时不支持换行

--- src/Lang/Let.hs
+++ src/Lang/Let.hs
@@ -8,7 +8,7 @@ newtype Identity = Identity String deriving (Show, Eq)
 data Expr = ENum Int
           | EDiff Expr Expr
           | EZero Expr
-          | EIF Expr Expr Expr
+          | EIf Expr Expr Expr
           | EVar Identity
           | ELet Identity Expr Expr
   deriving (Show, Eq)
@@ -26,12 +26,26 @@ ediff :: Parser Expr
 ediff = char '-' >> spaces >> brackets
   (EDiff <$> expr <* spaces <* char ',' <* spaces <*> expr)
 
+eif :: Parser Expr
+eif = EIf <$> keyExpr "if" <*> keyExpr "then" <*> keyExpr "else"
+
+keyParser :: String -> Parser a -> Parser a
+keyParser key a = string key >> spaces *> a <* spaces
+
+keyExpr :: String -> Parser Expr
+keyExpr key = keyParser key expr
+
 ezero :: Parser Expr
 ezero = string "zero?" >> spaces >> brackets (EZero <$> expr)
 
 evar :: Parser Expr
 evar = EVar <$> (Identity <$> many1 letter)
 
+elet :: Parser Expr
+elet = ELet <$> keyParser "let" (Identity <$> many1 letter) <*>
+       keyExpr "=" <*> keyExpr "in"
+
 -- parseTest expr "-(55, -(x,11))"
+-- parseTest expr "let z = 5 in let x = 3 in let y = -(x,1) in let x = 4 in -(z, -(x,y))"
 expr :: Parser Expr
-expr = choice [try enum, try ediff, try ezero, evar]
+expr = choice [try enum, try ediff, try ezero, try eif, try elet, evar]
