5094ff76b85faeab3ebd2b69d85f62dcd1a3a6f2
Liu Enze <liuenze6516@gmail.com>
2018-07-05 17:45:00 +0800

    finish let...

--- src/Lang/Let.hs
+++ src/Lang/Let.hs
@@ -3,8 +3,10 @@ module Lang.Let where
 import Text.Parsec
 import Text.Parsec.ByteString
 import Text.Parsec.Number
+import Data.Default
+import Data.Map.Strict(Map, (!), insert)
 
-newtype Identity = Identity String deriving (Show, Eq)
+newtype Identity = Identity String deriving (Show, Eq, Ord)
 data Expr = ENum Int
           | EDiff Expr Expr
           | EZero Expr
@@ -49,3 +51,32 @@ elet = ELet <$> keyParser "let" (Identity <$> many1 letter) <*>
 -- parseTest expr "let z = 5 in let x = 3 in let y = -(x,1) in let x = 4 in -(z, -(x,y))"
 expr :: Parser Expr
 expr = choice [try enum, try ediff, try ezero, try eif, try elet, evar]
+
+
+newtype Env = Env (Map Identity Val) deriving (Show, Eq)
+data Val = ValI Int | ValB Bool deriving (Show, Eq)
+
+instance Default Env where
+  def = Env def
+
+-- eval <$> runParser expr () "123" "let z = 5 in let x = 3 in let y = -(x,1) in let x = 4 in -(z, -(x,y))"
+eval :: Expr -> Val
+eval = evalEnv def
+
+evalEnv :: Env -> Expr -> Val
+evalEnv _ (ENum i) = ValI i
+evalEnv env (EDiff e1 e2) =
+  let (ValI i1) = evalEnv env e1
+      (ValI i2) = evalEnv env e2
+  in ValI (i1 - i2)
+evalEnv env (EZero e) =
+  if ValI 0 == evalEnv env e then ValB True else ValB False
+
+evalEnv env (EIf e1 e2 e3) =
+  let (ValB pred') = evalEnv env e1
+  in if pred' then evalEnv env e2 else evalEnv env e3
+
+evalEnv (Env curEnv) (EVar ident) = curEnv ! ident
+evalEnv env@(Env curEnv) (ELet ident e1 e2) =
+  let nextEnv = insert ident (evalEnv env e1) curEnv
+  in evalEnv (Env nextEnv) e2
