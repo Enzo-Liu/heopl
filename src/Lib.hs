105fdd0450a9a872befd2cf2c0b920b6137b418d
Liu Enze <liuenze6516@gmail.com>
2018-07-26 15:33:02 +0800

    add some test...

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,4 +3,20 @@ module Lib
     ) where
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  print $ maxSumOfList [1,-2,3,10,-4,7,2,-5]
+  print $ maxProdOfList [-2.5,4,0,3,0.5,8,-1]
+
+maxSumOfList :: [Int] -> (Int,Int)
+maxSumOfList = flip foldr (minBound,0) $
+  \cur (allMax,lastMax) ->
+    let newlast = max cur (lastMax+cur)
+        newmax = max allMax newlast in (newmax, newlast)
+
+maxProdOfList :: [Double] -> (Double, Double, Double)
+maxProdOfList = flip foldr (0,1,1) $
+  \cur (allMax,lastMax,lastMin) ->
+    let choices = [cur,lastMax * cur,lastMin * cur]
+        newLastMax = maximum choices
+        newLastMin = minimum choices
+        newMax = max allMax newLastMax in (newMax, newLastMax, newLastMin)
