8d498161a46b33c2f04cb635554f4a4f1baac3ec
Liu Enze <liuenze6516@gmail.com>
2018-07-31 12:47:29 +0800

    update

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,11 +7,14 @@ someFunc = do
   print $ maxSumOfList [1,-2,3,10,-4,7,2,-5]
   print $ maxProdOfList [-2.5,4,0,3,0.5,8,-1]
 
-maxSumOfList :: [Int] -> (Int,Int)
-maxSumOfList = flip foldr (minBound,0) $
-  \cur (allMax,lastMax) ->
+maxSumOfList :: [Int] -> (Int, Int, [Int])
+maxSumOfList = flip foldr (minBound, 0, []) $
+  \cur (allMax,lastMax, ls) ->
     let newlast = max cur (lastMax+cur)
-        newmax = max allMax newlast in (newmax, newlast)
+        newmax = max allMax newlast in
+      if newlast == cur
+      then (newmax, newlast, [cur])
+      else (newmax, newlast, ls++[cur])
 
 maxProdOfList :: [Double] -> (Double, Double, Double)
 maxProdOfList = flip foldr (0,1,1) $
